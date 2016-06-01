c984539a7bfd7d94b90653fec341497a8b5e8172
Joaozinho <jg-pain@hotmail.com>
2016-06-01 14:02:58 -0300

    inserindo item filmes na home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,5 +8,6 @@
         <li class="list-group-item">  <a href="{{ url('/Category/category/') }}">Categorias</a></li>
         <li class="list-group-item">  <a href="{{ url('/Actor/actors/') }}">Atores</a></li>
         <li class="list-group-item">  <a href="{{ url('/Language/language/') }}">Idiomas</a></li>
+        <li class="list-group-item">  <a href="{{ url('/Film/film/') }}">Filmes</a></li>
     </ul>
 @endsection
